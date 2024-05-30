d69bda83f216ddd467285870d4b759551196d2d4
Eero Vilpponen <eero.vilpponen@gmail.com>
2024-05-30 19:25:07 +0000

    WIP version working

--- src/main/java/me/eerovil/betterpeaceful/BetterPeaceful.java
+++ src/main/java/me/eerovil/betterpeaceful/BetterPeaceful.java
@@ -1,27 +1,41 @@
 package me.eerovil.betterpeaceful;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftLivingEntity;
 import org.bukkit.damage.DamageSource;
 // import org.bukkit.craftbukkit.v1_20_R4.entity.CraftLivingEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.minecraft.world.entity.EntityInsentient;
+
 import org.bukkit.inventory.ItemStack;
 // import net.minecraft.world.entity.EntityInsentient;
 // import net.minecraft.world.entity.ai.goal.PathfinderGoalRandomStrollLand;
 // import net.minecraft.world.entity.ai.goal.PathfinderGoalSelector;
 import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
+import org.bukkit.material.Colorable;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 
 public class BetterPeaceful extends JavaPlugin implements Listener {
@@ -69,34 +83,30 @@ public class BetterPeaceful extends JavaPlugin implements Listener {
     //     nmsEntity.bP.a(0, new PathfinderGoalRandomStrollLand(nmsEntity, 1.0));
     // }
 
-    @EventHandler
-    public void onEntityDamage(EntityDamageByEntityEvent event) {
-        event.setCancelled(true);
+    public static void showHearts(Entity entity) {
+        Location location = entity.getLocation();
+        entity.getWorld().spawnParticle(Particle.HEART, location.getX(), location.getY() + 1, location.getZ(), 2, 0.5, 0.5, 0.5, 0.0);
     }
 
-    // private fun LivingEntity.randomLoot(player: ServerPlayer): ItemStack? {
-    //     val lootContext = LootContext.Builder(player.getLevel())
-    //         .withRandom(java.util.Random())
-    //         .withParameter(LootContextParams.THIS_ENTITY, this)
-    //         .withParameter(LootContextParams.ORIGIN, position())
-    //         .withParameter(LootContextParams.DAMAGE_SOURCE, DamageSource.GENERIC)
-    //         .withParameter(LootContextParams.LAST_DAMAGE_PLAYER, player)
-    //         .withOptionalParameter(LootContextParams.KILLER_ENTITY, player)
-    //         .withOptionalParameter(LootContextParams.DIRECT_KILLER_ENTITY, player)
-    //         .create(LootContextParamSets.ENTITY)
-
-    //     return player.server.lootTables[lootTable].getRandomItems(lootContext).randomOrNull()
-    // }
-
-    // private void randomLoot(LivingEntity livingEntity) {
-    //     livingEntity.getLooyt
-    // }
-
-
     @EventHandler
-    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        Entity entity = event.getRightClicked();
-        if (!(entity instanceof LivingEntity)) return;
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        // Remove all damage events
+        event.setCancelled(true);
+        
+        Entity entity = event.getEntity();
+        if (!(entity instanceof LivingEntity)){
+            return;
+        }
+
+        Entity damagingEntity = event.getDamager();
+        // If damager is not a player, cancel the event
+        if (damagingEntity.getType() != EntityType.PLAYER) {
+            // Print log
+            return;
+        }
+
+        // Cast damaging entity to a player
+        Player player = (Player) damagingEntity;
 
         LivingEntity livingEntity = (LivingEntity) entity;
         UUID entityId = livingEntity.getUniqueId();
@@ -110,12 +120,45 @@ public class BetterPeaceful extends JavaPlugin implements Listener {
         }
 
         // Print a message to the player
-        event.getPlayer().sendMessage("You have interacted with " + livingEntity.getName());
+        // player.sendMessage("You have interacted with " + livingEntity.getName());
 
-        // Drop the items the entity would drop on death
-        // for (ItemStack drop : livingEntity.getLootTable().populateLoot(new org.bukkit.loot.LootContext.Builder(livingEntity.getLocation()).build())) {
-        //     livingEntity.getWorld().dropItemNaturally(livingEntity.getLocation(), drop);
-        // }
+        NamespacedKey key = new NamespacedKey("minecraft", "entities/" + livingEntity.getType().getKey().getKey());
+
+        LootTable lootTable = Bukkit.getLootTable(key);
+        if (lootTable == null) {
+            getLogger().info("Loot table not found: " + key);
+            return;
+        }
+
+        LootContext lootContext = new LootContext.Builder(livingEntity.getLocation())
+                .lootedEntity(livingEntity)
+                .killer(player)
+                .lootingModifier(1)
+                .luck(1)
+                .build();
+
+        Random random = new Random();
+        // print the table
+        Collection<ItemStack> loot = lootTable.populateLoot(random, lootContext);
+        for (ItemStack item : loot) {
+            player.sendMessage("You got " + item.getAmount() + " " + item.getType());
+            // Drop the item to the ground
+            livingEntity.getWorld().dropItemNaturally(livingEntity.getLocation(), item);
+        }
+        // If the entity was a sheep, drop wool according to its color
+        if (livingEntity instanceof Sheep) {
+            Colorable colorable = (Colorable) livingEntity;
+            Material wool = Material.valueOf(colorable.getColor().name() + "_WOOL");
+            ItemStack woolStack = new ItemStack(wool, 1);
+            player.sendMessage("You got " + woolStack.getAmount() + " " + woolStack.getType());
+            livingEntity.getWorld().dropItemNaturally(livingEntity.getLocation(), woolStack);
+        }
+
+        // Show hearts on the entity like when breeding
+        showHearts(livingEntity);
+
+
+        // Don't use livingEntity.getLootTable() because it's not available in 1.16
 
         interactCooldown.put(entityId, currentTime);
     }
